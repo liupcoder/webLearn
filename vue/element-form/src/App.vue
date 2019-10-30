cc80a677022c994865b252c7b688c3b2ff8fbafe
lp <liupcoder@gmail.com>
2019-10-30 22:33:45 +0800

    element-ui 组件

---
+++ vue/element-form/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    {{value}}
+    <k-input :value="value" @input="value = arguments[0]"></k-input>
+    <div>
+      <h3>Element表单</h3>
+      <hr />
+      <el-form :model="model" :rules="rules" ref="loginForm">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="model.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="password">
+          <el-input type="password" v-model="model.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('loginForm')">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import KInput from "./components/Input.vue";
+
+export default {
+  name: "App",
+  components: {
+    KInput
+  },
+  data() {
+    return {
+      value: "",
+      model: { username: "tom", password: "" },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名" },
+          { min: 6, max: 10, message: "请输入6~10的用户名" }
+        ],
+        password: [{ required: true, message: "请输入密码" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(form) {
+      this.$refs[form].validate(valid => {
+        if (valid) {
+          alert("请求登录!");
+        } else {
+          alert("校验失败！");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/element-form/src/components/Input.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" :value="value" @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  methods: {
+    onInput(e) {
+      let value = e.target.value;
+      this.$emit("input", value);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
